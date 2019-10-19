79f584a9abb7d26bed3c038bd0d9e54a3d1aeb99
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-19 14:27:00 +0100

    added trumpet tests

--- src/test/java/TrumpetTest.java
+++ src/test/java/TrumpetTest.java
@@ -18,4 +18,42 @@ public class TrumpetTest {
         assertEquals("brass", trumpet1.getType());
     }
 
+    @Test
+    public void getColour(){
+        assertEquals("yellow", trumpet1.getColour());
+    }
+
+    @Test
+    public void setColour(){
+        trumpet1.setColour("green");
+        assertEquals("green", trumpet1.getColour());
+    }
+
+    @Test
+    public void getStockPrice(){
+        assertEquals(50, trumpet1.getStockPrice());
+    }
+
+    @Test
+    public void setStockPrice(){
+        trumpet1.setStockPrice(40);
+        assertEquals(40, trumpet1.getStockPrice());
+    }
+
+    @Test
+    public void getSellPrice(){
+        assertEquals(100, trumpet1.getSellPrice());
+    }
+
+    @Test
+    public void setSellPrice(){
+        trumpet1.setSellPrice(110);
+        assertEquals(110, trumpet1.getSellPrice());
+    }
+
+    @Test
+    public void getNumOfValves(){
+        assertEquals(3, trumpet1.getNumOfValves());
+    }
+
 }
